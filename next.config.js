7e20d88c463dfacb78f4a980dc2b4acafe4bc3ca
Yasen <xiaoqizhao@outlook.com>
2023-01-21 11:54:05 -0500

    add image unoptimized in next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    unoptimized: true, // added 2023-01-21 for disable this as it's conflict with `next export`
+  },
 }
 
 module.exports = nextConfig
